f452e2dd7d916ddd6991477639378029d043105d
hamzah <hamzahnuralfalah@gmail.com>
2025-04-08 09:16:05 +0700

    mass update of data (DTO)

--- src/Data/SupplierData.php
+++ src/Data/SupplierData.php
@@ -8,21 +8,19 @@ use Spatie\LaravelData\Attributes\MapInputName;
 use Spatie\LaravelData\Attributes\MapName;
 
 class SupplierData extends Data implements DataSupplierData{
-    public function __construct(
-        #[MapName('id')] 
-        #[MapInputName('id')] 
-        public mixed $id = null,
+    #[MapName('id')] 
+    #[MapInputName('id')] 
+    public mixed $id = null;
 
-        #[MapName('name')] 
-        #[MapInputName('name')] 
-        public string $name,
+    #[MapName('name')] 
+    #[MapInputName('name')] 
+    public string $name;
 
-        #[MapName('phone')] 
-        #[MapInputName('phone')] 
-        public ?string $phone = null,
+    #[MapName('phone')] 
+    #[MapInputName('phone')] 
+    public ?string $phone = null;
 
-        #[MapName('address')] 
-        #[MapInputName('address')] 
-        public ?string $address = null
-    ){}
+    #[MapName('address')] 
+    #[MapInputName('address')] 
+    public ?string $address = null;
 }
