56b2799d04293688510444541aea3df8c0181f4c
Hamzah <hamzahnafalah@gmail.com>
2025-07-13 11:32:04 +0700

    update modules

--- src/Data/SupplierData.php
+++ src/Data/SupplierData.php
@@ -6,4 +6,8 @@ use Hanafalah\ModuleOrganization\Data\OrganizationData;
 use Hanafalah\ModuleProcurement\Contracts\Data\SupplierData as DataSupplierData;
 
 class SupplierData extends OrganizationData implements DataSupplierData{
+    public static function before(array &$attributes){
+        $attributes['flag'] ??= 'Supplier';
+        parent::before($attributes);
+    }
 }
